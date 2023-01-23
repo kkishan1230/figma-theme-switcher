e1bb3ab100173bb3839889b7a17b0df8f18793d1
kkishan1230 <kkishan1230@gmail.com>
2023-01-23 15:29:43 +0530

    footer done

--- src/components/footer-links/Footer.jsx
+++ src/components/footer-links/Footer.jsx
@@ -1,40 +1,65 @@
 import React from "react";
 import FooterLinks from "./FooterLinks";
+import pin_location from "../../images/map_white.svg";
+import phone from "../../images/phone_white.svg";
+import twitter from "../../images/twitter_white.svg";
+import fb from "../../images/fb_white.svg";
+import linked from "../../images/in_white.svg";
 
 const Footer = () => {
   return (
     <div className="py-[150px] bg-[#252B42]">
       <div className="max-w-[991px] flex justify-between mx-auto">
-        <div className="flex">
-          <FooterLinks
-            links_head="Pages"
-            link1="Home"
-            link2="Product"
-            link3="Pricing"
-            link4="About"
-            link5="Contact"
-            w={170}
-          />
-          <FooterLinks
-            className="tomothy"
-            links_head="Tomothy"
-            link1="Eleanor Edwards"
-            link2="Ted Robertson"
-            link3="Annette Russell"
-            link4="Jennie Mckinney"
-            link5="Gloria Richards"
-            w={186}
-          />
-          <FooterLinks
-            className="jane"
-            links_head="Jane Black"
-            link1="Philip Jones"
-            link2="Product"
-            link3="Colleen Russell"
-            link4="Marvin Hawkins"
-            link5="Bruce Simmmons"
-            w={187}
-          />
+        <FooterLinks
+          links_head="Pages"
+          link1="Home"
+          link2="Product"
+          link3="Pricing"
+          link4="About"
+          link5="Contact"
+        />
+        <FooterLinks
+          className="tomothy"
+          links_head="Tomothy"
+          link1="Eleanor Edwards"
+          link2="Ted Robertson"
+          link3="Annette Russell"
+          link4="Jennie Mckinney"
+          link5="Gloria Richards"
+        />
+        <FooterLinks
+          className="jane"
+          links_head="Jane Black"
+          link1="Philip Jones"
+          link2="Product"
+          link3="Colleen Russell"
+          link4="Marvin Hawkins"
+          link5="Bruce Simmmons"
+        />
+        <div className="pl-[50px]">
+          <div className="flex items-center gap-4 ml-[-5px]">
+            <div className="w-[40px]">
+              <img src={pin_location} alt="" />
+            </div>
+            <p className="footer_links">7480 Mockingbird Hill undefined</p>
+          </div>
+          <div className="flex items-center gap-4 mt-[21px]">
+            <div className="w-[20px]">
+              <img src={phone} alt="" />
+            </div>
+            <p className="footer_links">(239) 555-0108</p>
+          </div>
+          <div className="flex gap-[33px] mt-[42px] items-center">
+            <div>
+              <img src={twitter} alt="" />
+            </div>
+            <div>
+              <img src={fb} alt="" />
+            </div>
+            <div>
+              <img src={linked} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
