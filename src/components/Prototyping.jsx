62247746b47738d7cd81aedf20085b3aacddba82
kkishan1230 <kkishan1230@gmail.com>
2023-01-23 09:56:05 +0530

    removed unuseed components

--- src/components/Prototyping.jsx
+++ src/components/Prototyping.jsx
@@ -1,4 +1,3 @@
-import { TextField } from "@mui/material";
 import React from "react";
 import proto_img from "../images/letter.png";
 
--- src/components/YoutubePlayer.jsx
+++ src/components/YoutubePlayer.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 import video from "../video/video.mp4";
 import ReactPlayer from "react-player";
-import Playbtn from "./Playbtn";
 
 const YoutubePlayer = () => {
-  const opts = {
-    playerVars: {
-      controls: 0,
-    },
-  };
-
   return (
     <div>
       <ReactPlayer url={video} muted={true} controls={true} />
